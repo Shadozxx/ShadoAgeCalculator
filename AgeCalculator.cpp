7cbeebc278cead4f3741b8342d2345899ec14b79
Shado <tvoi_fsbshnik@outlook.com>
2024-11-30 13:48:13 +0300

    Версия 1.0

---
+++ AgeCalculator.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+	int age;
+	system("chcp 1251 > nul");
+	cout << "Калькулятор возраста от Shado" << endl;
+	cout << "Введите ваш возраст > ";
+	cin >> age;
+	cout << "\nДумаем..." << endl;
+	Sleep(2000);
+	system("cls");
+	cout << "Ваш возраст: " << age << " лет" << endl;
+	if (age <= 6) {
+		cout << "Завтра в садик, вырубаем комп...";
+		system("shutdown /s /t 5");
+	}
+	else {
+		cout << "Проверка завершена! Можно закрыть программу...";
+		system("pause > nul");
+	}
+	return 0;
+}
